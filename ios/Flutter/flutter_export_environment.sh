b24933f2297184078aa4783d6624dd510053848e
JK TAN <jktan@JKs-MacBook-Pro.local>
2019-12-21 09:54:39 +0800

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jktan/Documents/Softwares/Mobile/Flutter/sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/jktan/Documents/workplaces/Train/Mobile/Flutter/Udemy/Maximilian/native-features/native-10-finished"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jktan/Documents/Softwares/Mobile/Flutter/sdk/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
